260bc70452496e21bbb679cbe925f187e8f46aba
Akmmonir <akmmonirulhasan@gmail.com>
2022-01-09 11:28:49 +0600

    Another modification in MyFancyJavaProject

--- src/myfancyjavaproject/MyFancyJavaProject.java
+++ src/myfancyjavaproject/MyFancyJavaProject.java
@@ -6,9 +6,11 @@ public class MyFancyJavaProject {
     class TestClass{
         int id;
         String name;
+        String address;
         TestClass(int id,String name){
             this.id=id;
             this.name=name;
+            this.address=address;
         }
     }
     
